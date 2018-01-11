44ef4505d5ab93aa17dd83eafe267f1f47a753dc
Peter Morton <darthpemo@hotmail.com>
2018-01-11 15:33:13 -0500

    Initial commit

---
+++ src/clojure_rest_test/handler.clj
@@ -0,0 +1,12 @@
+(ns clojure-rest-test.handler
+  (:require [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+
+(defroutes app-routes
+           (GET "/" [] "Hello World")
+           (GET "/test" [] "Hello Testing World")
+           (route/not-found "Not Found"))
+
+(def app
+  (wrap-defaults app-routes site-defaults))
